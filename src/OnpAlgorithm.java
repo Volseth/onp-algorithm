531bfc035b9cadd251787bf7f3eb769c2bd4c301
Volseth <b.duda11@wp.pl>
2019-03-24 23:44:11 +0100

    feature("added basic classes")

--- src/OnpAlgorithm.java
+++ src/OnpAlgorithm.java
@@ -1,2 +1,12 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class OnpAlgorithm {
+    public void loadOperators(File f){
+
+    }
+    public void calculateFormula(){
+        Scanner scanner=new Scanner(System.in);
+        
+    }
 }
