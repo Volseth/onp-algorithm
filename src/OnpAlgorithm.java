c57651ed2d21485f5868eed43a7ef7e2aa3394b0
Volseth <b.duda11@wp.pl>
2019-03-27 12:26:11 +0100

    feature("Main menu","partial of conversion infix to onp formula")

--- src/OnpAlgorithm.java
+++ src/OnpAlgorithm.java
@@ -1,12 +1,45 @@
+import java.io.BufferedReader;
 import java.io.File;
-import java.util.Scanner;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Stack;
 
 public class OnpAlgorithm {
-    public void loadOperators(File f){
+    ArrayList<String> formula;
+
+    public OnpAlgorithm(ArrayList<String> formula){
+        this.formula=formula;
 
     }
-    public void calculateFormula(){
-        Scanner scanner=new Scanner(System.in);
-        
+    public static ArrayList<String> loadFormulas(String filepath) {
+        ArrayList<String> formula=new ArrayList<>();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(new File(filepath)))) {
+        while(bufferedReader.readLine()!=null){
+            formula.add(bufferedReader.readLine());
+        }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return formula;
+    }
+
+    public void converseFormula(){
+        ArrayDeque<String> numbersDeque=new ArrayDeque<>();
+        Stack<String> operatorsStack=new Stack<>();
+        ArrayList<String> onpFormulas=new ArrayList<>();
+        for (String s:formula ) {
+            String [] splitedString= s.split("");
+            for (int i=0;i<splitedString.length;i++){
+                if(splitedString[i].matches("/d")){
+                    numbersDeque.add(splitedString[i]);
+                }
+                if(splitedString[i].equals("(")){
+                    operatorsStack.push(splitedString[i]);
+                }
+
+            }
+        }
     }
 }
--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,39 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Test {
     public static void main(String[] args) {
-
+        ArrayList<String> formulasList=new ArrayList<>();
+        OnpAlgorithm onpAlgorithm=new OnpAlgorithm(formulasList);
+        Scanner scanner=new Scanner(System.in);
+        int choice=0;
+        System.out.println("Wybierz opcję:\n1.Wczytaj działania z klawiatury\n2.Wczytaj działania z pliku\n3.Wyjście");
+        choice=scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    System.out.println("Wprowadź działania. Słowo KONIEC kończy wpisywanie działań");
+                    boolean flag=true;
+                    while(flag){
+                        String formula= scanner.next();
+                        formulasList.add(formula);
+                        if(formula.equals("KONIEC")){
+                            flag=false;
+                            choice=0;
+                            onpAlgorithm.calculateFormula();
+                            break;
+                        }
+                    }
+                    break;
+                case 2:
+                    System.out.println("Podaj nazwę pliku tekstowego:");
+                    String filepath=scanner.nextLine();
+                    formulasList=OnpAlgorithm.loadFormulas(filepath);
+                    onpAlgorithm.calculateFormula();
+                    choice=0;
+                    break;
+                case 0:
+                    System.out.println("Wybierz opcję:\n1.Wczytaj działania z klawiatury\n2.Wczytaj działania z pliku\n3.Wyjście");
+                    break;
+            }
     }
 }
